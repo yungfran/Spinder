a4b8eeba85e4d22a2652522a9d10d67498f4318d
Francis Tran <francuhz4@gmail.com>
2023-08-08 17:23:20 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
@@ -9,14 +11,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button>
+          Login
+        </button>
       </header>
     </div>
   );
---
+++ src/backend/Auth.js
@@ -0,0 +1,11 @@
+
+
+function Auth() {
+    CLIENT_ID = process.env.CLIENT_ID
+    CLIENT_SECRET = process.env.CLIENT_SECRET
+
+    
+
+}
+
+export default Auth;
