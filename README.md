524b2e2ffeecf3547a43dabba80acd832d05bde2
Francis Tran <francuhz4@gmail.com>
2023-08-24 18:00:32 -0500

    Get genre counts for a user.

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 Deliverables:
-    Read all Playlists
-    Look at songs of each playlist
-        How to analyze each song?
+    Read all from user Playlists ✅ 
+    Look at songs of each playlist ✅
+    Look at all songs from liked songs
+    Come up with "like" ranges for musical variables
+    Get a count with the user's genres
+    Calculate the user's avg/median value for musical variables
+    Search for new songs with the top genres 
+    Filter new songs to disregard songs and artists we've seen
+    Rank songs by how much the user is to like them
+    Do UI for desktop
+    Do UI for mobile
--- src/LoggedIn.js
+++ src/LoggedIn.js
@@ -5,11 +5,10 @@ function LoggedIn( props ) {
 
     const [accessToken,setAccess] = useState(props.access)
     const [refreshToken,setRefresh] = useState(props.refresh)
-    const [userSongs, setUserSongs] = useState([])
+    const [userSongs, setUserSongs] = useState(new Set()); /* Used to calculate musical score avgs*/
+    const [userArtists,setUserArtists] = useState(new Map()); /* Used for genre counts*/
+    const [genreCounts, setGenreCounts] = useState(new Map());
     
-  
-    
-
     /* Begin Access functions */
     async function checkToken () {
         const playlistURI =  "https://api.spotify.com/v1/me/playlists?limit=50"
@@ -36,22 +35,20 @@ function LoggedIn( props ) {
      /* End Access functions */
 
      /* Begin parsing user data */
-     /* Store all playlistIds */
+     /* Stores all playlistIds and sends to parsePlaylists() */
     async function getPlaylists() {
         const userPlaylistURI =  "https://api.spotify.com/v1/me/playlists?limit=50"
         const headers = {
             Authorization: 'Bearer ' + accessToken
         };
-        console.log(accessToken)
         const response = await axios.get(userPlaylistURI, {headers})
         let nextUserPlaylistURI = response.data.next;
-        console.log(response)
         let currPlaylistIDs = []
         response.data.items.forEach(playlist => currPlaylistIDs.push(playlist.tracks.href))
-        console.log(currPlaylistIDs)
         
+        /* As long as we have more requests make, send them*/
         if (nextUserPlaylistURI) {
-            const nextPlaylistResponse = await axios.get(userPlaylistURI, {headers})
+            const nextPlaylistResponse = await axios.get(nextUserPlaylistURI, {headers})
             nextUserPlaylistURI = nextPlaylistResponse.data.next
             nextPlaylistResponse.data.items.forEach( playlist => currPlaylistIDs.push(playlist.tracks.href))
         }
@@ -59,40 +56,179 @@ function LoggedIn( props ) {
         return currPlaylistIDs;
     }
 
+    /* Entry point into reading the user's playlists. Should only happen once*/
+    /* TODO: Fix getting artists and songs at the same time, best outcome in one call*/
     async function parsePlaylists() {
-        const playlists = await getPlaylists();
-        const headers = {
-            Authorization: 'Bearer ' + accessToken
-        };
-        let songs = await parsePlaylist(playlists[0])
-       
+        const seenUser = localStorage.getItem("songs");
+        const seenArtists = localStorage.getItem("albums")
+
+        console.log(accessToken)
+        if (seenArtists !== null){
+            console.log("getting all playlists")
+            const playlists = await getPlaylists();
+            const headers = {
+                Authorization: 'Bearer ' + accessToken
+            };
+            // Call parsePlaylist using map and get a promise
+            const parsePromises = playlists.map(playlist => parsePlaylist(playlist)); // Create an array of promises
+            
+            // Wait until every we finish every parsePlaylist call
+            await Promise.all(parsePromises);
+            
+            localStorage.setItem("songs",Array.from(userSongs))
+            const artistObject = Object.fromEntries(userArtists);
+            localStorage.setItem('artists', JSON.stringify(artistObject));
+
+        } else {
+            console.log("Grabbed playlists from local ")
+            const songs = seenUser.split(',');
+            setUserSongs(new Set(songs))
+
+
+            const retrievedMapString = localStorage.getItem('artists');
+            const retrievedMapObject = JSON.parse(retrievedMapString);
+            const retrievedMap = new Map(Object.entries(retrievedMapObject));
+            setUserArtists(retrievedMap)
+        }
+
+        // Now all parsePlaylist calls have finished, you can call your additional function here
+        await getGenres();
+        console.log(genreCounts);
     }
 
+    /* Gets all songs from a playlist and adds it to our songs set
+       Gets all albums of those songs and store the counts in our album map*/
+       /* WORKING ON GETTING ARTISTS INTO A LIST SO THAT WE CAN GET GENRES FROM IT*/
     async function parsePlaylist(playlistURI) {
         const headers = {
             Authorization: 'Bearer ' + accessToken
         };
-        const response = await axios.get(playlistURI, {headers})
-        console.log(response.data.items)
-        // response.data.items.forEach( trackData => 
-        //     {
-        //         trackData.track
-        //     }
-        // )
-        return;
+        const response = await axios.get(playlistURI, {headers});
+        setUserArtists(prevUserArtist => {
+            const newArtists = new Map(prevUserArtist);
+            console.log(response.data.items);
+            response.data.items.forEach(trackData => {
+            if (trackData.track !== null && trackData.track !== undefined) {
+                if (trackData.track.artists !== null && trackData.track.artists !== undefined) {
+                    if(trackData.track.artists[0] !== null ){
+                        const artistId = trackData.track.artists[0].id;
+                        if(artistId !== null){
+                            if (newArtists.has(artistId)) {
+                                newArtists.set(artistId, newArtists.get(artistId) + 1);
+                            } else {
+                                newArtists.set(artistId, 1);
+                            }
+                    }
+                 }
+                }
+            }
+            });
+            
+            setUserArtists(newArtists)
+            return newArtists
+          });
+
+
+        // setUserSongs(prevUserSongs => {
+        //     const newSongIds = response.data.items.map(trackData => trackData.track.id);
+        //     setUserSongs(new Set([...prevUserSongs, ...newSongIds]));
+        //     return new Set([...prevUserSongs, ...newSongIds]);
+        // });
     }
 
     /* End parsing user data*/
 
 
 
+    /* Calculate User Tastes */
+    async function getGenres() {
+        console.log("Calculating genres")
+        let albumsURI = "https://api.spotify.com/v1/artists?ids="
+        const headers = {
+            Authorization: 'Bearer ' + accessToken
+        };
 
+        const albumsPerRequest = 10;
+        const albumsList = Array.from(userArtists.keys())
+        const requestCount = Math.ceil(userArtists.size / albumsPerRequest);
+        for(let i = 0; i < requestCount; i += 1){
+            const currAlbumIDs = albumsList.slice(i, i + albumsPerRequest);
+            currAlbumIDs.forEach(albumId => albumsURI += albumId +",")
+            albumsURI = albumsURI.substring(0,albumsURI.length - 1)
+            const response = await axios.get(albumsURI,{headers})
+
+            const artists = response.data.artists;
+            console.log(artists)
+            const genrePromise = artists.map(artist => parseGenreFromArtist(artist));
+
+            albumsURI = "https://api.spotify.com/v1/artists?ids=";
+        }
+    }
+
+    /* Looks at a single artist and updates the current genre count for the user
+        For now pick genre[0] */
+    async function parseGenreFromArtist(artist) {
+        const genres = artist.genres
+
+        setGenreCounts( prevGenreCounts => {
+            let newGenreCounts = new Map(prevGenreCounts);
+            genres.map(genre => {
+                if (newGenreCounts.has(genre)){
+                    newGenreCounts.set(genre, newGenreCounts.get(genre) + 1)
+                } else {
+                    newGenreCounts.set(genre,1);
+                }
+            })
+            setGenreCounts(newGenreCounts)
+            return newGenreCounts
+        });
+
+    }
+
+    /* Calculates the user's favorite genres*/
+    /* Assumes an ID is always 22 characters + 1 character for the comma*/
+    async function calculateTaste () {
+       const songsPerRequest = 20
+       const allSongsString = localStorage.getItem("songs");
+       const startingSongURI = "https://api.spotify.com/v1/tracks?ids=" 
+       const charactersPerCall = 23 * songsPerRequest;
+       const numCalls = Math.ceil(allSongsString.length / 23 / songsPerRequest);
+       console.log(allSongsString)
+       // On the nth, cut it off at the length of allSongStrings
+       for(let offset = 0; offset < (numCalls - 1); offset += 1){
+        let start = offset * charactersPerCall;
+        let end = (offset + 1) * charactersPerCall - 1;
+
+        getSong(startingSongURI+allSongsString.substring(start,end));
+       }
+
+       //const songPromises = Array.from(userSongs).map(songId => getSong(songId))
+
+    }
+
+    /* Given a song's spotify URI, return the */
+    async function getSong (songURI) {
+        console.log("Grabbing data for "  + songURI)
+        const headers = {
+            Authorization: 'Bearer ' + accessToken
+        };
+        const response = await axios.get(songURI,{headers})
+        console.log(response.data.tracks[1].artists)
+    }
+
+    /* Analyzes the sounds the user likes. Danceability, Energy, etc*/
+    function calculateTastes() {
+
+    }
 
     
     useEffect( () => {
         refresh()
     },[])
 
+
+    // useEffect( () => {
+    // },[userSongs])
 
     return(
         <div className="Yak">
